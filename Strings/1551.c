25aba208b2bdb194fae6a87765498b65007e87a1
Carlos Eduardo Cavalcanti <69211390+carloseduardocsf@users.noreply.github.com>
2021-06-20 20:05:52 -0300

    .

--- Strings/1551.c
+++ Strings/1551.c
@@ -15,7 +15,7 @@ int main(){
 
         for(j = 0; frase[j] != '\0'; j++){
             for(i = 0; i < 26; i++){
-                if(frase[j] == alfabeto[i]){ // se a determinada letra da frase é igual a determinada letra do alfabeto
+                if(frase[j] == alfabeto[i]){ // se a determinada letra da frase Ã© igual a determinada letra do alfabeto
                     letras_iguais++;
                     alfabeto[i] = 0;
                 }
@@ -37,72 +37,6 @@ int main(){
     return 0;
 
 }
-
-
-
-
-
-
-
-
-
-
-
-/*int main(){
-
-    int N, i, quant_letras;
-    char frase[1004];
-
-
-    scanf("%d", &N);
-
-
-    while(N > 0){
-
-        fgets(frase, 1000, stdin); // comando para o usuario inserir a frase
-
-        for(i = 0; frase[i] != '\0'; i++){
-            printf("%s", frase[i]);
-        }
-
-
-        quant_letras =  strlen(frase); // ler quantas letras tem na frase
-
-        printf("A frase tem %s letras", quant_letras);
-
-        if(quant_letras == 26){
-            printf("frase completa");
-        }else if(quant_letras >= 13 && quant_letras < 26){
-            printf("frase quase completa");
-        }else{
-            printf("frase mal elaborada");
-        }
-
-        N--;
-    }
-
-    return 0;
-
-}*/
-
-
-
-
-/*int main(){
-
-    int  i;
-    char frase[1000];
-
-    printf("Digite uma frase: ");
-    fgets(frase, 1000, stdin);
-
-    printf("%s", frase);
-
-
-    return 0;
-
-}*/
-
 
 
 
