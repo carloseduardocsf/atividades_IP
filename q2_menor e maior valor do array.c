0a99a1819d19d693eb0470e60f99fbbf8c35216c
Carlos Eduardo Cavalcanti <69211390+carloseduardocsf@users.noreply.github.com>
2021-07-07 17:29:32 -0300

    .

---
+++ q2_menor e maior valor do array.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+
+/* implemente aqui sua funcao */
+
+// função para saber o menor valor do array
+int Menor(int arr[], int quant, int menor){
+    int i;
+    //encontra o valor mínimo
+    menor = arr[0];
+    for(i = 1; i < quant; i++){
+        if(arr[i] < menor){
+            menor = arr[i];
+        }
+    }
+    return menor;
+
+}
+
+// função para saber o maior valor do array
+int Maior(int arr[], int quant, int maior){
+    int i;
+    // encontra o valor maximo
+    maior = arr[0];
+    for(i = 1; i < quant; i++){
+        if(arr[i] > maior){
+            maior = arr[i];
+        }
+    }
+    return maior;
+
+}
+
+
+int main(void){
+    int ar[100];
+    int n, i, min, max;
+
+    scanf("%d", &n);
+    for(i = 0; i < n; i++){
+        scanf("%d", &ar[i]);
+    }
+
+    /* faca aqui a chamada a sua funcao*/
+    min = Menor(ar, n, min);
+    max = Maior(ar, n, max);
+    printf("%d %d\n", min, max);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
